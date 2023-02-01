af1080fa44aaa08d9bcd171e1c5c5d58bc2a83b6
Shay <shayafra@gmail.com>
2023-01-31 18:33:03 -0800

    added greatest common divisor but with no recursion

---
+++ greatestCommonDiviser.js
@@ -0,0 +1,28 @@
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+
+//no recursion
+function mygcd(x,y){
+    let common = null
+    let high = null
+    let low = null
+    let counter = 0
+    if(x == y){
+      return 1
+    }else if(x > y){
+      high = x
+      low = y
+    }else{
+      high = y
+      low = x
+    }
+    while(counter <= low){
+      if(high % counter == 0 && low % counter == 0){
+        common = counter
+      }
+      counter++
+    }
+    return common
+  }
